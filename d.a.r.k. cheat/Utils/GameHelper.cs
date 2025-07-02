74686c7b712f57c6dbe4db5c59126ffd24aba747
Nothh <68824550+iiNothh@users.noreply.github.com>
2025-07-02 15:13:30 +0300

    Delete d.a.r.k. cheat directory

--- d.a.r.k. cheat/Utils/GameHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GameHelper : MonoBehaviour
-{
-    public static new object FindObjectOfType(System.Type type)
-    {
-        return FindObjectOfType(type, true);
-    }
-}
